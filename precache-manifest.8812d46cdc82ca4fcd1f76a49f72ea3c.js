774065ccee3b3d7e77015e00ce77a04f4da5d8a0
Kell Banks <mckellbanks@gmail.com>
2020-07-18 14:14:58 -0600

    Updates

---
+++ precache-manifest.8812d46cdc82ca4fcd1f76a49f72ea3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894bfc91168094e5784799da277c50fa",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "f26c594454007f582548",
+    "url": "/memory-game/static/css/main.427007ab.chunk.css"
+  },
+  {
+    "revision": "faf51f69cb5a198a49d1",
+    "url": "/memory-game/static/js/2.0070a718.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.0070a718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26c594454007f582548",
+    "url": "/memory-game/static/js/main.acc1acc9.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
